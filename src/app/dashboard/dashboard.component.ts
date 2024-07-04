4f45b3cea699f187a7f459cf550c7285c7f0f132
Rxkzz <rizkialhabsi6@gmail.com>
2024-07-04 17:01:46 +0700

    fix log out

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../user/user';
+import { UserAuthService } from '../user-auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  user!:User
+ 
+  constructor(public userAuthService: UserAuthService, private router: Router) {}
+ 
+  ngOnInit(): void {
+   if(localStorage.getItem('token') == "" || localStorage.getItem('token') == null){
+      this.router.navigateByUrl('/')
+    }else {
+      this.userAuthService.getUser().then(({data})=>{
+        this.user = data;
+      })
+    }
+  }
+ 
+  logoutAction () {
+    this.userAuthService.logout().then(()=>{
+      localStorage.setItem('token', "")
+      this.router.navigateByUrl('/')
+    }).catch(()=>{
+      localStorage.setItem('token', "")
+      this.router.navigateByUrl('/')
+    })
+   
+  }
 }
+
